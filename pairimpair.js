29b70ab4131788a756b08db71c90959706d36ea7
alirezaalavi7713 <alirezaalavi7713@gmail.com>
2025-06-04 10:05:43 +0200

    k

---
+++ pairimpair.js
@@ -0,0 +1,6 @@
+function verifierparite(nombre) {
+    if(nombre % 2===0) 
+        {return "Paire";}
+    else {return "impaire";}
+}
+console.log(verifierparite(8)) 
---
+++ plusgrand.js
@@ -0,0 +1,15 @@
+function afficherplusgrand() {
+    const n1 = 15;
+    const n2 = 15;
+
+if (n1 > n2){
+    console.log('Le plus grand est :' + n1);
+ } else if (n2 > n1) {
+        console.log('Le plus grand est : ' + n2);
+    } else {
+        console.log("Les nombres sont egaux");
+    }
+}
+
+afficherplusgrand();
+
